503ed20ac13eebee31c7d140be89bf035a4b6332
Georgiy <goshazvir@gmail.com>
2014-09-08 23:59:25 +0300

    upd chat

--- .build/js/common.js
+++ .build/js/common.js
@@ -13,23 +13,37 @@ head.ready(function() {
 	});
 
 	var windowheight = $(window).height();
-	var chatheight = (windowheight - 180);
+	var chatheight = (windowheight);
+	var chatheight2 = (windowheight);
 	$(".js-chat-set-height").css('min-height', chatheight);
+	$(".js-chat-set-height2").css('min-height', chatheight - 250);
 
 
 	function chat_fixed() {
 		var position = $(".cb-nav-filter").offset().top;
-		var position_2 = $(".chat__fixed").offset().top;
-
-		if ($(window).scrollTop() > position) {
-		  $(".chat__fixed").addClass('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+		var position_2 = $(".chat").offset().top;
+		if ($(window).scrollTop() >= position) {
+		  $(".chat__fixed").addClass("js-fixed");
 		  // alert(position);
 		}
-		if ($(window).scrollTop() <= position_2) {
-		  $(".chat__fixed").removeClass('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+		if ($(window).scrollTop() < position_2) {
+		  $(".chat__fixed").removeClass("js-fixed");
 		  // alert(position_2);
 		}
 	}
-	chat_fixed();
+	if ($(".cb-nav-filter").length > 0){
+	  chat_fixed();
+	}
 
+
+	$(window).resize(function(){
+	  if ($(".cb-nav-filter").length > 0){
+	  	chat_fixed();
+	  }
+	});
+	$(window).scroll(function(){
+	  if ($(".cb-nav-filter").length > 0){
+	  	chat_fixed();
+		}
+	});
 });
