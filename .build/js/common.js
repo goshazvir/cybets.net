1e3c60bcfa7ef1dd0006b74eb85a12145002edb6
Georgiy <goshazvir@gmail.com>
2014-09-10 12:24:44 +0300

    upd

--- .build/js/common.js
+++ .build/js/common.js
@@ -45,6 +45,29 @@ head.ready(function() {
 	  chat_fixed();
 	}
 
+	function modals() {
+		$(document).on("click", function(){
+			$(".js-overlay").hide();
+			$("body").removeClass("has-open-popup");
+		});
+		$(".js-popup-link").on("click", function(event){
+			$(".js-overlay").fadeOut(200);
+			var popup = $(this).attr("href");
+			$("body").addClass("has-open-popup");
+			$("."+popup).parent().fadeIn(200);
+			event.stopPropagation();
+			return false;
+		});
+		$(".js-popup-close").on("click", function(){
+			$(".js-overlay").fadeOut(200);
+			$("body").removeClass("has-open-popup")
+			return false;
+		});
+		$(".js-popup").on("click", function(event){
+			event.stopPropagation();
+		});
+	} modals();
+
 
 	$(window).resize(function(){
 	  if ($(".cb-nav-filter").length > 0){
