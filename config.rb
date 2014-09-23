ee6ad9c56a12a80259bde2a71cddde1070e5bb74
Georgiy <goshazvir@gmail.com>
2014-09-23 21:12:49 +0300

    refactoring1

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = ".build/css"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = ".build/img"
-javascripts_dir = ".build/js"
+images_dir = "img"
+javascripts_dir = "js"
 
 
 # You can select your preferred output style here (can be overridden via the command line):
---
+++ js/init.js
@@ -0,0 +1,2 @@
+head.load("js/lib/jquery.js", "js/lib/bootstrap.min.js", "js/lib/bootstrap.min.js","js/lib/baron.full.js", "js/common.js");
+
